eb8e06152bee3ff1124d94eb8f8372d26f5d424f
Taranov Sergiy <scorp1323@gmail.com>
2020-06-18 22:26:07 +0300

    'First

---
+++ www/index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+
+for( $i = 0;  $i <= 5; $i++  ) {
+
+    switch ($i) {
+        case 1:
+            case 2:
+                echo '<br>' . $i;
+            break;
+        case 3:
+            break;
+    }
+}
+
+
+
+
+
+
+
+
+
+?>
