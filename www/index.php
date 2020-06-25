6f853e577eb014162d287c3e07ee44fd12bdb5e0
Taranov Sergiy <scorp1323@gmail.com>
2020-06-25 21:10:44 +0300

    'css+Бутстрап'

--- www/index.php
+++ www/index.php
@@ -1,11 +1,15 @@
-
+<?php
+$cssVersion = '3.4.2'; ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Cache-Control" content="no-cache">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css?v=<?php echo $cssVersion; ?>" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
 <body>
 
@@ -31,7 +35,6 @@ include_once 'pages/right_column.php';
 //футер
 include_once 'pages/footer.php';
 ?>
-
 
 
 </body>
